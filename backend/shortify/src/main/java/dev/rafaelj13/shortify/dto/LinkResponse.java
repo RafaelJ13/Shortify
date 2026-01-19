a367116550b9ce6c3e64b64c4976fe3f8bec9f73
RafaelJ13 <rjanuario@ipcbcampus.pt>
2026-01-19 23:11:01 +0000

    removido o id do DTO de resposta quando encurtado um link e criado as requests necessarias da api no insomnia

--- backend/shortify/src/main/java/dev/rafaelj13/shortify/dto/LinkResponse.java
+++ backend/shortify/src/main/java/dev/rafaelj13/shortify/dto/LinkResponse.java
@@ -2,14 +2,12 @@ package dev.rafaelj13.shortify.dto;
 
 public class LinkResponse {
     private String shortUrl;
-    private Integer id;
 
     public LinkResponse() {
     }
 
     public LinkResponse(String shortUrl, Integer id) {
         this.shortUrl = shortUrl;
-        this.id = id;
     }
 
     public String getShortUrl() {
@@ -20,11 +18,4 @@ public class LinkResponse {
         this.shortUrl = shortUrl;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
